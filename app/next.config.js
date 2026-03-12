a7f4eca5255c3e89263a60f3e998b509a6c3e6dc
shaposhnikmcd-code <Shaposhnik.mcd@gmail.com>
2026-03-12 08:35:38 +0100

    Fix Prisma external packages config

--- app/next.config.js
+++ app/next.config.js
@@ -6,6 +6,9 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  experimental: {
+    serverComponentsExternalPackages: ['@prisma/client', 'bcryptjs'],
+  },
 };
 
 module.exports = nextConfig;
