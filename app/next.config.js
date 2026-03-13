d6f58c7df353a3b12faaaca1ea616365243cb214
shaposhnikmcd-code <Shaposhnik.mcd@gmail.com>
2026-03-13 23:20:34 +0100

    fix: nextauth url config

--- app/next.config.js
+++ app/next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   typescript: { ignoreBuildErrors: true },
   eslint: { ignoreDuringBuilds: true },
+  env: {
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL || "https://dr-shaposhnik-platform.vercel.app",
+  },
   images: {
     remotePatterns: [
       {
@@ -12,8 +15,17 @@ const nextConfig = {
         protocol: "https",
         hostname: "w4p-merch.s3.eu-central-1.amazonaws.com",
       },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "graph.facebook.com",
+      },
     ],
   },
 };
 
 module.exports = nextConfig;
+```
